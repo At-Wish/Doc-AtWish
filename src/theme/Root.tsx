498f45ef46e95834794d453d91fb9762389a77e0
shantanukhond <shantanukhond@gmail.com>
2025-10-23 20:28:30 +0530

    clarity added

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -11,6 +11,18 @@ const Root: React.FC<PropsWithChildren<{}>> = ({ children }) => {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9396682749552228"
           crossOrigin="anonymous"
         ></script>
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "tuq2r37epx");
+            `,
+          }}
+        />
       </Head>
       {children}
     </>
